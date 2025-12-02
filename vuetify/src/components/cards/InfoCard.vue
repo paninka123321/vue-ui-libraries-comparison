783e30158cc62dc462707668aef90c888a405779
paninka123321 <paulinka_k2000@wp.pl>
2025-12-02 12:57:46 +0100

    add notebook

--- vuetify/src/components/cards/InfoCard.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <v-card class="mx-auto my-4" max-width="300" elevation="4">
-    <v-card-title class="justify-center">Info Card</v-card-title>
-    <v-card-text class="text-center">
-      <p>This is a simple information card with some text content.</p>
-    </v-card-text>
-  </v-card>
-</template>
--- vuetify/src/components/cards/MultiOptCard.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <v-card class="mx-auto my-4" max-width="300" elevation="5">
-    <v-card-title class="justify-center">Newsletter</v-card-title>
-    <v-card-text class="text-center">
-      <p>Subscribe to our newsletter to get the latest updates!</p>
-      <v-btn color="orange" class="mt-2">Subscribe</v-btn>
-    </v-card-text>
-  </v-card>
-</template>
